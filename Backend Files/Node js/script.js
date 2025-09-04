b31d6db9adf3459f51bdc9cbe84e5f8fc43549bc
Codingcycle121 <paragbansal120@gmail.com>
2025-09-04 09:52:02 +0530

    module.exports use

--- Backend Files/Node js/script.js
+++ Backend Files/Node js/script.js
@@ -1,5 +1,8 @@
-let args=process.argv;
-for(let i=2; i<args.length; i++)
-{
-    console.log("hello to ",args[i]);
-}
+// let args=process.argv;
+// for(let i=2; i<args.length; i++)
+// {
+//     console.log("hello to ",args[i]);
+// }
+let math=require("./math");
+console.log(math.sum(5,5));
+console.log(math.PI);
